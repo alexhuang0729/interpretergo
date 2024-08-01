0ef1dea8c3ace6f9cc852628debc1a6ca7b64b5a
alexhuang0729 <ah4631227@gmail.com>
2024-07-31 19:30:36 -0500

    Added some forgotten stuff. Need to fix tests.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -35,6 +35,31 @@ func TestReturnStatement(t *testing.T) {
 		}
 	}
 }
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not %T. got=%T", &ast.LetStatement{}, s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+		return false
+	}
+
+	return true
+}
 func checkParserErrors(t *testing.T, p *Parser) {
 	errors := p.Errors()
 	if len(errors) == 0 {
